71e5d118111d2bfa99e34ba355e8514493237570
JHONNY CHIPANA CHOQUE <quebuenoesta88@gmail.com>
2026-05-14 23:55:08 -0400

    Update mi_ia_web.py

--- mi_ia_web.py
+++ mi_ia_web.py
@@ -2,8 +2,6 @@ import os
 import streamlit as st
 import google.generativeai as genai
 
-
-
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 
 st.set_page_config(page_title="IA de Emociones", page_icon="🌈")
@@ -16,9 +14,8 @@ if st.button("Enviar a la IA"):
     if usuario_input:
         with st.spinner("Pensando..."):
             try:
-                # Usamos el modelo directo
-               model = genai.GenerativeModel("gemini-2.0-flash")
-               response = model.generate_content(usuario_input)
+                model = genai.GenerativeModel("gemini-2.0-flash")
+                response = model.generate_content(usuario_input)
                 st.success("Un mensaje para ti:")
                 st.write(response.text)
             except Exception as e:
