80f6adab685b8272b7d432f0fd7d5121b533eb99
JHONNY CHIPANA CHOQUE <quebuenoesta88@gmail.com>
2026-05-14 23:26:35 -0400

    Update mi_ia_web.py

--- mi_ia_web.py
+++ mi_ia_web.py
@@ -2,7 +2,7 @@ import streamlit as st
 from google import genai
 
 # Ponemos la llave directamente aquí para que no haya pierde
-llave = "AIzaSyD-4k8hI71raLjXBDieUNxTjkrxhFp85qU"
+llave = "AIzaSyB_hVxdR6sOOUCeR6XFQwcf4HIX0Pw1Idk"
 
 client = genai.Client(api_key=llave)
 
